a32941a3f2a36ac9c7e382fa4ec50073eb9dc434
Jin <202@careernet.org.tw>
2023-08-23 10:56:56 +0800

    新增JS內容

---
+++ js/all.js
@@ -0,0 +1,10 @@
+var $.ajax({
+    type: "POST",
+    contentType: "application/json; charset=utf-8",
+    dataType: "dataType",
+    url: "url",
+    data: "data",
+    success: function (response) {
+        
+    }
+});
